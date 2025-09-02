918e1299a726f4881350aeabe2263ce46b244cce
Ayush Gupta <ayush@apptile.io>
2025-09-02 19:29:31 +0530

    DSf

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -8,30 +8,16 @@ interface LogoProps {
 }
 
 export const Logo: React.FC<LogoProps> = ({ text, href }) => {
-  const [isMobile, setIsMobile] = useState(false);
+  
 
-  useEffect(() => {
-    const checkIfMobile = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    // Check on initial load
-    checkIfMobile();
-
-    // Add event listener for window resize
-    window.addEventListener('resize', checkIfMobile);
-
-    // Cleanup event listener
-    return () => window.removeEventListener('resize', checkIfMobile);
-  }, []);
 
   return (
     <div className="header__logo">
       <a href={href}>
         <img 
-          width={isMobile ? 50 : 200} 
-          height={isMobile ? 50 : 70} 
-          src={isMobile ? mobile_logo : laptoo_logo} 
+          width={window.innerWidth <= 768 ? 50 : 200} 
+          height={window.innerWidth <= 768 ? 50 : 70} 
+          src={window.innerWidth <= 768? mobile_logo : laptoo_logo} 
           alt={text}
           className="header__logo-img"
         />
