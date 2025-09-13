d5574842394faa8bb02abc931ec05acf5a200031
Ayush Gupta <ayush@apptile.io>
2025-09-14 01:10:17 +0530

    added:

--- src/constants/CarouselData.tsx
+++ src/constants/CarouselData.tsx
@@ -8,28 +8,29 @@ export const carouselImages = [
   },
   {
     id: '2',
-    src: 'https://i.pinimg.com/1200x/bd/f9/52/bdf952fe521f1232076d029ec75890d0.jpg',
+    src: 'https://i.pinimg.com/1200x/a0/6a/6e/a06a6e9fd8689aff56f7c516d7c121d8.jpg',
     alt: 'Elegant wedding invitation',
     title: 'Elegant Digital Invitations',
     subtitle: 'Beautifully crafted designs for your special celebration'
   },
   {
     id: '3',
-    src: 'https://i.pinimg.com/1200x/ba/a7/79/baa779d70a78ceb788a081403e920a20.jpg',
+    src: 'https://i.pinimg.com/1200x/3d/63/69/3d6369708b351b7fd4253d43432e5591.jpg',
     alt: 'Wedding celebration',
     title: 'Celebrate Love',
     subtitle: 'Every love story deserves a beautiful beginning'
   },
   {
     id: '4',
-    src: 'https://images.unsplash.com/photo-1692715984205-451b1055249e?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    src: 'https://i.pinimg.com/736x/ef/a8/de/efa8def907afb477f785fadbbdcb3f89.jpg',
     alt: 'Wedding flowers and decoration',
     title: 'Stunning Designs',
     subtitle: 'Professional templates that capture your unique style'
   },
   {
     id: '5',
-    src: 'https://i.pinimg.com/1200x/53/35/fb/5335fb9c1de851faec8ddc2dd3d6f744.jpg',
+    src: ' https://images.unsplash.com/photo-1692715984205-451b1055249e?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+   
     alt: 'Wedding rings and flowers',
     title: 'Timeless Memories',
     subtitle: 'Share your joy with friends and family in style'
