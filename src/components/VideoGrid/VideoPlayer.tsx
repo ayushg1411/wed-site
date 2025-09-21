c05c257204f0395b5d8b59f6d30f0031baeeaf80
Ayush Gupta <ayush@apptile.io>
2025-09-21 21:12:16 +0530

    base

--- src/components/VideoGrid/VideoPlayer.tsx
+++ src/components/VideoGrid/VideoPlayer.tsx
@@ -47,6 +47,8 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({
             break;
           case 'play':
             setIsPlaying(true);
+              sendCommand('setVolume', 1);
+              setIsMuted(false);
             currentlyPlayingVideo = vimeoId;
             break;
           case 'pause':
@@ -161,7 +163,7 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({
       <div className="video-player__wrapper">
         <iframe
           ref={iframeRef}
-          src={`https://player.vimeo.com/video/${vimeoId}?api=1&background=0&controls=${isInAppBrowser() ? 1 : 0}&title=0&byline=0&portrait=0&autopause=0&muted=0&autoplay=0`}
+          src={`https://player.vimeo.com/video/${vimeoId}?api=1&background=0&controls=${isInAppBrowser() ? 1 : 0}&title=0&byline=0&portrait=0&autoplay=0`}
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowFullScreen
