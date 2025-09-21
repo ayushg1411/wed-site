47bb4c6b8bbf182af7709a46587aa6611433b000
Ayush Gupta <ayush@apptile.io>
2025-09-21 21:04:35 +0530

    base

--- src/components/VideoGrid/VideoPlayer.tsx
+++ src/components/VideoGrid/VideoPlayer.tsx
@@ -161,7 +161,7 @@ export const VideoPlayer: React.FC<VideoPlayerProps> = ({
       <div className="video-player__wrapper">
         <iframe
           ref={iframeRef}
-          src={`https://player.vimeo.com/video/${vimeoId}?api=1&background=0&controls=${isInAppBrowser() ? 1 : 0}&title=0&byline=0&portrait=0&autopause=0&autoplay=0`}
+          src={`https://player.vimeo.com/video/${vimeoId}?api=1&background=0&controls=${isInAppBrowser() ? 1 : 0}&title=0&byline=0&portrait=0&autopause=0&muted=0&autoplay=0`}
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture"
           allowFullScreen
